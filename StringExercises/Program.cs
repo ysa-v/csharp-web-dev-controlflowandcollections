bf224c9e926c3d9286e56042eaf4f4b5758e3b41
ysa-v <ysa.vandenberg@gmail.com>
2022-08-09 13:29:59 -0500

    Split Suess quote into arrays

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,15 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            string suessQuote = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+            string[] suessSplit1 = suessQuote.Split(" ");
+
+            Console.WriteLine(string.Join(",", suessSplit1));
+
+            string[] suessSplit2 = suessQuote.Split(". ");
+
+            Console.WriteLine(string.Join(",", suessSplit2));
         }
     }
 }

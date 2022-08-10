d2e09fed11d6472477a30968a495b578954648d8
ysa-v <ysa.vandenberg@gmail.com>
2022-08-09 20:17:30 -0500

    List manipulation methods and search function created

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ListExercises
 {
@@ -6,7 +7,43 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Lists!");
+            static int SumOfEvenNums(List<int> list)
+            {
+                int sum = 0;
+                foreach (int num in list)
+                {
+                    if (num % 2 == 0)
+                    {
+                        sum += num;
+                    }
+                }
+                return sum;
+            }
+
+            List<int> nums = new List<int> { 32, 25, 74, 33, 22, 43, 10, 2, 7, 35 };
+
+            Console.WriteLine(SumOfEvenNums(nums));
+
+            static void PrintFiveLetter(List<string> list, int num)
+            {
+                foreach (string word in list)
+                {
+                    if (word.Length == num)
+                    {
+                        Console.WriteLine(word);
+                    }
+                }
+            }
+
+            List<string> words = new List<string> { "hello", "nope", "extended", "fives" };
+
+            Console.WriteLine("Enter the word length for the search: ");
+            string input = Console.ReadLine();
+            int wordLength = int.Parse(input);
+
+            PrintFiveLetter(words, wordLength);
+
+
         }
     }
 }
